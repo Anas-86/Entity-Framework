1e500139f5fa5f6359f3ff323906cc231e85c6c6
Anas Al Kala <anas.kala@hotmail.com>
2023-12-01 16:17:37 +0100

    Adding the method OnModelCreating() in the db_context class to use Fluent API.

--- EF_Experimental/Northwind.cs
+++ EF_Experimental/Northwind.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,24 @@ using System.Threading.Tasks;
 
 namespace EF_Experimental
 {
-    public class Northwind : DbContext
+    public partial class Northwind : DbContext
     {
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Server=localhost;Database=Northwind;User Id=sa;Password=Hitman4719781978;";
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+                .Property(category => category.CategoryName)
+                .IsRequired()       // Not Null
+                .HasMaxLength(15);
         }
     }
 }
